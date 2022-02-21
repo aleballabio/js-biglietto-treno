1abfbb700292b8aaf3ae192ffc0f1d5f880de603
aleballabio <aleballabio05@gmail.com>
2022-02-21 16:34:54 +0100

    Rounded to 2 decimal and working on all discounts

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+const etaOld = 65;
+const etaYoung = 17;
+const prezzoalKm = 0.21;
+const scontoOld = 40;
+const scontoYoung = 20;
+
+let userEtà = prompt('Quanti anni hai?');
+let userKm = prompt('Quanti KM hai percorso (usare i . se in decimale)?');
+
+let prezzoScont = 0;
+
+let prezzoTotale = userKm * prezzoalKm;
+prezzoTotale = prezzoTotale.toFixed(2)
+
+let outputElement = document.getElementById('biglietto-treno-prezzo');
+
+if (userEtà >= etaOld) {
+    prezzoScont = prezzoTotale - (prezzoTotale * scontoOld) / 100;
+    prezzoScont = prezzoScont.toFixed(2)
+    outputElement.innerHTML = ('Il prezzo del suo biglietto è di' + ' ' + prezzoScont);
+} else if (userEtà <= etaYoung) {
+    prezzoScont = prezzoTotale - (prezzoTotale * scontoYoung) / 100;
+    prezzoScont = prezzoScont.toFixed(2)
+    outputElement.innerHTML = ('Il prezzo del suo biglietto è di' + '  ' + prezzoScont);
+} else {
+    outputElement.innerHTML = ('Il prezzo del suo biglietto è di' + ' ' + prezzoTotale);
+}
